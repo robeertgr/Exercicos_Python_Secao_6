324fd45a98d1130fd5fbf6592499f7fd3088d98d
Robert Granja <robeertgr@live.com>
2021-12-13 14:45:23 -0300

    Exercício resolvido

---
+++ Exercicio_3.py
@@ -0,0 +1,11 @@
+"""
+Faça um algoritmo utilizando o comando while que mostra
+uma mensagem regressiva na tela, iniciando em 10 e terminando
+em 0. Mostrar uma mensagem "Fim!" após a contagem
+"""
+
+n = 10
+while n != 0:
+    print(n)
+    n = n - 1
+print("Fim!")
