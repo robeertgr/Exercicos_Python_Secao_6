72d4e1f8fda52247dbad001e13eb616bf0610b05
Robert Granja <robeertgr@live.com>
2021-12-17 15:03:49 -0300

    Exercício resolvido

--- Exercicio_14.py
+++ Exercicio_14.py
@@ -1,11 +1,11 @@
 """
 Faça um programa que leia um número inteiro positivo par N e imprima todos os números
-pares de 0 até N em ordem crescente
+pares de N até 0 em ordem decrescente.
 """
 
 num = int(input("Digite um número par: "))
 while num % 2 != 0:
     num = int(input("Número não é par. Digite um número par: "))
 if num % 2 == 0:
-    for count in range(0, num + 2, 2):
+    for count in range(num, -2, -2):
         print(count)
