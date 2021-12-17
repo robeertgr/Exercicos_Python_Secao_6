2f28ac39e1a5eeb6dca48d5dcb4677064aa32bc5
Robert Granja <robeertgr@live.com>
2021-12-17 15:02:05 -0300

    Correção do enunciado do exercício

--- Exercicio_12.py
+++ Exercicio_12.py
@@ -1,6 +1,6 @@
 """
 Faça um programa que leia um número inteiro positivo N e imprima todos os números
-naturais de 0 até N em ordem decrescente.
+naturais de N até 0 em ordem decrescente.
 """
 
 num = int(input("Digite um número: "))
